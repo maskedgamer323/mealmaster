fc380f940fcd360dbe49932cfe697dca172b31c6
maskedgamer323 <151095026+maskedgamer323@users.noreply.github.com>
2025-10-25 01:55:15 -0500

    Replace global styles with Tailwind CSS

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,4 @@
-// pages/_app.js
-import '../styles/globals.css';
-
+import '../styles/tailwind.css'
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <Component {...pageProps} />
 }
