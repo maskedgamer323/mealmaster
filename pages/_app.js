67bc8b4a8e19d454376c8d3dbf852a8cac9c174f
maskedgamer323 <151095026+maskedgamer323@users.noreply.github.com>
2025-10-25 01:47:17 -0500

    Update Tailwind CSS imports in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
-import 'tailwindcss/tailwind.css'
+import 'tailwindcss/base.css'
+import 'tailwindcss/components.css'
+import 'tailwindcss/utilities.css'
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
